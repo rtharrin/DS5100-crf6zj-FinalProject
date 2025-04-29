7eea1f66b1e2e1e7f8385f963422efc9cac6f6d6
rtharrin <rtharrin@outlook.com>
2025-04-29 15:55:22 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='monte-carlo',
+    name='montecarlo',
     version='1.0.0',
     description='A Monte Carlo simulator package with Die, Game, and Analyzer classes',
     url='https://github.com/rtharrin/DS5100-crf6zj-FinalProject',
     author='Ryan Harrington',  
     license='MIT',
-    packages=['monte-carlo'],
+    packages=['montecarlo'],
     install_requires=[
         'numpy',
         'pandas'
